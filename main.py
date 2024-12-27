17a8facadeaf34af9fa9a0ee81a3f5a2be02961b
Linda Fitt <lindafittr@gmail.com>
2024-12-27 15:24:11 -0800

    Adds meteors, laser and laser movement and timers

--- main.py
+++ main.py
@@ -9,6 +9,17 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_frect(center = (WINDOW_WIDTH/2,WINDOW_HEIGHT/2))
         self.direction = pygame.math.Vector2(0,0)
         self.speed = 300
+
+        # cooldown
+        self.can_shoot = True
+        self.laser_shoot_time = 0
+        self.cooldown_duration = 400
+
+    def laser_timer(self):
+        if not self.can_shoot:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.laser_shoot_time >= self.cooldown_duration:
+                self.can_shoot = True
     
     def update(self, dt):
          # input
@@ -20,8 +31,18 @@ class Player(pygame.sprite.Sprite):
         self.rect.center += self.direction * self.speed * dt   
 
         recent_keys = pygame.key.get_just_pressed()
-        if recent_keys[pygame.K_SPACE]: 
-            print("fire laser")
+        if recent_keys[pygame.K_SPACE] and self.can_shoot: 
+            laser = Laser(all_sprites, player, laser_surf)
+            self.can_shoot = False
+            self.laser_shoot_time = pygame.time.get_ticks()
+            
+        self.laser_timer()
+        # if self.rect.right >= WINDOW_WIDTH or self.rect.left <= 0:
+        #     self.direction[0] *= -1
+    
+        # if self.rect.bottom >= WINDOW_HEIGHT or self.rect.top <= 0:
+        #     self.direction[1] *= -1
+        
 
 class Star(pygame.sprite.Sprite):
     def __init__(self, groups, surf):
@@ -29,7 +50,27 @@ class Star(pygame.sprite.Sprite):
         self.image = surf
         self.rect = self.image.get_frect(center = (randint(0,WINDOW_WIDTH),randint(0,WINDOW_HEIGHT)))
         
+class Meteor(pygame.sprite.Sprite):
+    def __init__(self, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load(join('images', 'meteor.png')).convert_alpha()
+        self.rect = self.image.get_frect(center = (randint(0,WINDOW_WIDTH),randint(0,WINDOW_HEIGHT)))
 
+class Laser(pygame.sprite.Sprite):
+    def __init__(self, groups, player, surf):
+        super().__init__(groups)
+        self.player = player
+        player_x, player_y = self.player.rect.center
+        self.image = surf
+        self.rect = self.image.get_frect(midbottom = (player_x , player_y - 50))
+        self.speed = 300
+    
+
+    def update(self, dt):
+        self.direction = pygame.math.Vector2(0,0)
+        self.direction.y = -1
+        self.direction = self.direction.normalize() if self.direction else self.direction
+        self.rect.center += self.direction * self.speed * dt 
 
 
 # setting up our basics
@@ -47,17 +88,15 @@ x = 100
 all_sprites = pygame.sprite.Group()
 
 star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
+laser_surf = pygame.image.load(join('images', 'laser.png')).convert_alpha()
 for i in range(20):
     stars = Star(all_sprites, star_surf)
+   
 player = Player(all_sprites)
 
-meteor_surf = pygame.image.load(join('images', 'meteor.png')).convert_alpha()
-meteor_rect = meteor_surf.get_frect(center = (WINDOW_WIDTH/2,WINDOW_HEIGHT/2))
-
-laser_surf = pygame.image.load(join('images', 'laser.png')).convert_alpha()
-laser_rect = laser_surf.get_frect(bottomleft = (20,WINDOW_HEIGHT - 20))
-
-
+# custom events -> meteor event
+meteor_event = pygame.event.custom_type()
+pygame.time.set_timer(meteor_event,500)
 
 while running:
     dt = clock.tick() / 1000
@@ -66,22 +105,17 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
+        if event.type == meteor_event:
+            meteor = Meteor(all_sprites) 
+    #update
     all_sprites.update(dt)
 
     # draw the game
     display_surface.fill('darkgray')
     all_sprites.draw(display_surface)
-    
-    
-
+        
     # player movement
-    # player_rect.center += player_direction * player_speed * dt
-    # if player_rect.right >= WINDOW_WIDTH or player_rect.left <= 0:
-    #     player_direction[0] *= -1
     
-    # if player_rect.bottom >= WINDOW_HEIGHT or player_rect.top <= 0:
-    #     player_direction[1] *= -1
     
     pygame.display.update()
 pygame.quit()
